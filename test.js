a3e13ab2458d9c4aeada9c6bd4860688bd70c05b
brooklyn-ai <jordan@laf.land>
2019-01-28 14:08:38 -0500

    add node modules, compass sass

--- test.js
+++ test.js
@@ -21,8 +21,51 @@ test('responds to requests', (t) => {
       // Successful response
       t.equal(response.statusCode, 200);
       // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
+      t.notEqual(body.indexOf("<title>Movie Info App</title>"), -1);
+      t.notEqual(body.indexOf("Built with Node on Heroku"), -1);
     });
   });
+});
+
+
+//Origional code 
+
+
+// var express = require('express');
+// var app = express();
+
+// app.set ('port', (process.env.PORT || 3000));
+// app.use(express.static(path.join(__dirname, '/public')));
+
+// var fs = require('fs');
+// var path = require('path');
+// var http = require('http');
+
+
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.use('/', express.static(path.join(__dirname, 'public'));
+
+// app.get('/favorites', function(req, res){
+//   var data = fs.readFileSync('./data.json');
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// };
+
+// app.get('favorites', function(req, res){
+//   if(!req.body.name || !req.body.oid){
+//     res.send("Error");
+//     return
+  
+//   var data = JSON.parse(fs.readFileSync('./data.json'));
+//   data.push(req.body);
+//   fs.writeFile('./data.json', JSON.stringify(data));
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// }});
+
+app.list(3000, function(){
+  console.log("Listening on port 3000");
 });
